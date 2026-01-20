c265e2e92fdd5fe7a3a8547ec9c47cc8c9ce08af
matteo-blasoni <matteo.blasoni.99@gmail.com>
2026-01-20 16:48:56 +0900

    init proj

---
+++ AtlasSDK.cpp
@@ -0,0 +1,12 @@
+﻿// AtlasSDK.cpp : Defines the entry point for the application.
+//
+
+#include "AtlasSDK.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
