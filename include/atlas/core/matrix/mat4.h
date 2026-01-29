81d6fd14e3d48657b114a9872784bb54d86736ad
matteo-blasoni <matteo.blasoni.99@gmail.com>
2026-01-29 16:24:54 +0900

    update matrix module: added trs functions to compute inverse on trs matrix; added trs functions to decompose trs matrix;

--- include/atlas/core/matrix/mat4.h
+++ include/atlas/core/matrix/mat4.h
@@ -34,8 +34,19 @@ namespace atlas::core::mat4 {
     Mat4 rotate(const vec::Vec3 &r);
     Mat4 rotate(float x, float y, float z);
 
+    // TRS specific functions
+
     Mat4 TRS(const vec::Vec3 &t, const quat::Quat &r, const vec::Vec3 &s);
     Mat4 TRS(const vec::Vec3 &t, const vec::Vec3 &r, const vec::Vec3 &s);
+
+    Mat4 inverseTRS(const Mat4& m);
+    Mat4 inverseTRS(const vec::Vec3& t, const quat::Quat& r, vec::Vec3& s);
+
+    vec::Vec3 getTranslation(const Mat4 &trs);
+    quat::Quat getRotation(const Mat4 &trs);
+    vec::Vec3 getScale(const Mat4 &trs);
+
+    void decomposeTRS(const Mat4& m, vec::Vec3& outT, quat::Quat& outR, vec::Vec3& outS);
 
     vec::Vec3 transformPoint(const Mat4 &mat, const vec::Vec3 &v);
     vec::Vec3 transformDirection(const Mat4 &mat, const vec::Vec3 &v);
--- src/core/matrix/mat4.cpp
+++ src/core/matrix/mat4.cpp
@@ -1,3 +1,4 @@
+#include <cmath>
 #include <atlas/core/matrix/mat4.h>
 
 namespace atlas::core::mat4 {
@@ -112,6 +113,110 @@ namespace atlas::core::mat4 {
     Mat4 TRS(const vec::Vec3 &t, const vec::Vec3 &r, const vec::Vec3 &s) {
         quat::Quat q = quat::fromEuler(r);
         return TRS(t, q, s);
+    }
+
+    Mat4 inverseTRS(const vec::Vec3& t, const quat::Quat& r, const vec::Vec3& s)
+    {
+        const vec::Vec3 invS{
+            1.0f / s.x,
+            1.0f / s.y,
+            1.0f / s.z
+        };
+
+        const quat::Quat invR = quat::conjugate(r);
+
+        const vec::Vec3 invT = -quat::rotate(t * invS, invR);
+
+        return TRS(invT, invR, invS);
+    }
+
+    vec::Vec3 getTranslation(const Mat4& trs)
+    {
+        return {
+            trs(0, 3),
+            trs(1, 3),
+            trs(2, 3)
+        };
+    }
+
+    quat::Quat getRotation(const Mat4& trs)
+    {
+        vec::Vec3 xAxis{ trs(0,0), trs(0,1), trs(0,2) };
+        vec::Vec3 yAxis{ trs(1,0), trs(1,1), trs(1,2) };
+        vec::Vec3 zAxis{ trs(2,0), trs(2,1), trs(2,2) };
+
+        const float sx = xAxis.length();
+        const float sy = yAxis.length();
+        const float sz = zAxis.length();
+
+        if (sx == 0.0f || sy == 0.0f || sz == 0.0f)
+            return quat::identity();
+
+        xAxis /= sx;
+        yAxis /= sy;
+        zAxis /= sz;
+
+        // Row-major rotation matrix
+        const float m00 = xAxis.x, m01 = xAxis.y, m02 = xAxis.z;
+        const float m10 = yAxis.x, m11 = yAxis.y, m12 = yAxis.z;
+        const float m20 = zAxis.x, m21 = zAxis.y, m22 = zAxis.z;
+
+        quat::Quat q;
+        const float trace = m00 + m11 + m22;
+
+        if (trace > 0.0f)
+        {
+            const float s = std::sqrt(trace + 1.0f) * 2.0f;
+            q.w = 0.25f * s;
+            q.x = (m21 - m12) / s;
+            q.y = (m02 - m20) / s;
+            q.z = (m10 - m01) / s;
+        }
+        else if (m00 > m11 && m00 > m22)
+        {
+            const float s = std::sqrt(1.0f + m00 - m11 - m22) * 2.0f;
+            q.w = (m21 - m12) / s;
+            q.x = 0.25f * s;
+            q.y = (m01 + m10) / s;
+            q.z = (m02 + m20) / s;
+        }
+        else if (m11 > m22)
+        {
+            const float s = std::sqrt(1.0f + m11 - m00 - m22) * 2.0f;
+            q.w = (m02 - m20) / s;
+            q.x = (m01 + m10) / s;
+            q.y = 0.25f * s;
+            q.z = (m12 + m21) / s;
+        }
+        else
+        {
+            const float s = std::sqrt(1.0f + m22 - m00 - m11) * 2.0f;
+            q.w = (m10 - m01) / s;
+            q.x = (m02 + m20) / s;
+            q.y = (m12 + m21) / s;
+            q.z = 0.25f * s;
+        }
+
+        return q.normalized();
+    }
+
+    vec::Vec3 getScale(const Mat4& trs)
+    {
+        const vec::Vec3 xAxis{ trs(0,0), trs(0,1), trs(0,2) };
+        const vec::Vec3 yAxis{ trs(1,0), trs(1,1), trs(1,2) };
+        const vec::Vec3 zAxis{ trs(2,0), trs(2,1), trs(2,2) };
+
+        return {
+            xAxis.length(),
+            yAxis.length(),
+            zAxis.length()
+        };
+    }
+
+    void decomposeTRS(const Mat4& m, vec::Vec3& outT, quat::Quat& outR, vec::Vec3& outS) {
+        outT = getTranslation(m);
+        outR = getRotation(m);
+        outS = getScale(m);
     }
 
     vec::Vec3 transformPoint(const Mat4 &mat, const vec::Vec3 &v) {
